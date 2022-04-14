0955cf58ba277fa4fbd017bb4bd7c3ebbb407fc8
Paulo Henrique Da Silva Neto <paulo.silva1989@ibm.com>
2022-04-14 11:38:51 -0300

    MySQL connection

--- src/main/java/ibm/itau/projetobancocentral/AppConfig/AppConfig.java
+++ src/main/java/ibm/itau/projetobancocentral/AppConfig/AppConfig.java
@@ -5,18 +5,8 @@ import ibm.itau.projetobancocentral.repositories.DadosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
 @Configuration
 public class AppConfig implements CommandLineRunner {
     @Autowired
@@ -28,36 +18,13 @@ public class AppConfig implements CommandLineRunner {
     String url;
 
     @Override
-    public void run(String... args)  {
+    public void run(String... args) {
 
-        System.out.println("URL: " + url);
+        Dados[] arraysDeDados = restTemplate.getForObject(url, Dados[].class);
 
-//        List<Map<String,String>> listadeDados = restTemplate.getForObject(url, List.class);
-        Dados[] arraysDeDados= restTemplate.getForObject(url, Dados[].class);
-        Arrays.toString(arraysDeDados);
-        for(Dados d: arraysDeDados){
-            System.out.println(d);
-            dadosRepository.save(d);
+        for (Dados d : arraysDeDados)  dadosRepository.save(d);
 
-        }
-
-//
-//
-//
-//        for (int i = 0; i < listadeDados.size(); i++) { // Loop para criacao de objetos
-//
-//           System .out.println(listadeDados.get(i).get("data")+listadeDados.get(i).get("valor")); // Printa os dados da API
-//
-//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // Formata a data
-//            LocalDate data = LocalDate.parse(listadeDados.get(i).get("data"),formatter); // Converte a data para LocalDate
-//
-//            BigDecimal valor = new BigDecimal(listadeDados.get(i).get("valor")); // Converte o valor para BigDecimal
-//
-//            dadosRepository.save(new Dados(data,valor)); // Salva os dados no banco de dados H2
-//
-//
-
-        }
 
     }
+}
 
--- src/test/java/ibm/itau/projetobancocentral/ProjetoBancoCentralApplicationTests.java
+++ src/test/java/ibm/itau/projetobancocentral/ProjetoBancoCentralApplicationTests.java
@@ -1,13 +1,27 @@
 package ibm.itau.projetobancocentral;
 
+import ibm.itau.projetobancocentral.entities.Dados;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 @SpringBootTest
 class ProjetoBancoCentralApplicationTests {
-
     @Test
     void contextLoads() {
+        assertEquals(1,1);
+        BigDecimal valor = new BigDecimal(100);
+        LocalDate data = LocalDate.now();
+        Dados dado = new Dados(data, valor);
+        assertEquals(LocalDate.now(), dado.getData());
+
+
+
     }
 
 }
